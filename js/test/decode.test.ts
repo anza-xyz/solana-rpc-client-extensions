bfa10a6ec524db8332190dd43757b213ade725df
Jon C <me@jonc.dev>
2024-08-07 23:10:46 +0200

    JS: Lint and format properly, add lockfile

--- js/test/decode.test.ts
+++ js/test/decode.test.ts
@@ -2,39 +2,37 @@ import test from 'ava';
 import { stakeAccountCodec, stakeHistoryCodec } from '../src';
 
 const data = new Uint8Array([
-    1, 0, 0, 0, 128, 213, 34, 0, 0, 0, 0, 0, 133, 0, 79, 231, 141, 29, 73, 61, 232, 35,
-    119, 124, 168, 12, 120, 216, 195, 29, 12, 166, 139, 28, 36, 182, 186, 154, 246, 149,
-    224, 109, 52, 100, 133, 0, 79, 231, 141, 29, 73, 61, 232, 35, 119, 124, 168, 12, 120,
-    216, 195, 29, 12, 166, 139, 28, 36, 182, 186, 154, 246, 149, 224, 109, 52, 100, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 128, 213, 34, 0, 0, 0, 0, 0, 133, 0, 79, 231, 141, 29, 73, 61,
+  232, 35, 119, 124, 168, 12, 120, 216, 195, 29, 12, 166, 139, 28, 36, 182, 186,
+  154, 246, 149, 224, 109, 52, 100, 133, 0, 79, 231, 141, 29, 73, 61, 232, 35,
+  119, 124, 168, 12, 120, 216, 195, 29, 12, 166, 139, 28, 36, 182, 186, 154,
+  246, 149, 224, 109, 52, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0,
 ]);
 
 const stakeHistoryData = new Uint8Array([
-    1, 0, 0, 0, 0, 0, 0, 0,
-    2, 0, 0, 0, 0, 0, 0, 0,
-    3, 0, 0, 0, 0, 0, 0, 0,
-    4, 0, 0, 0, 0, 0, 0, 0,
-    5, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 4, 0,
+  0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0,
 ]);
 
 test('decode stake', (t) => {
-    // As long as we get the 4-byte enum and the first field right, then
-    // we're sure the rest works out
-    const unpacked = stakeAccountCodec.decode(data);
-    t.is(unpacked.meta.rentExemptReserve, 2282880n);
+  // As long as we get the 4-byte enum and the first field right, then
+  // we're sure the rest works out
+  const unpacked = stakeAccountCodec.decode(data);
+  t.is(unpacked.meta.rentExemptReserve, 2282880n);
 });
 
 test('decode stake history', (t) => {
-    // As long as we get the 4-byte enum and the first field right, then
-    // we're sure the rest works out
-    const unpacked = stakeHistoryCodec.decode(stakeHistoryData);
-    t.is(unpacked.length, 1);
-    t.is(unpacked[0].epoch, 2n);
-    t.is(unpacked[0].effective, 3n);
-    t.is(unpacked[0].activating, 4n);
-    t.is(unpacked[0].deactivating, 5n);
+  // As long as we get the 4-byte enum and the first field right, then
+  // we're sure the rest works out
+  const unpacked = stakeHistoryCodec.decode(stakeHistoryData);
+  t.is(unpacked.length, 1);
+  t.is(unpacked[0].epoch, 2n);
+  t.is(unpacked[0].effective, 3n);
+  t.is(unpacked[0].activating, 4n);
+  t.is(unpacked[0].deactivating, 5n);
 });
--- js/test/delegation.test.ts
+++ js/test/delegation.test.ts
@@ -2,136 +2,156 @@ import test from 'ava';
 import { getStakeActivatingAndDeactivating } from '../src';
 
 export interface StakeHistoryEntry {
-    epoch: bigint;
-    effective: bigint;
-    activating: bigint;
-    deactivating: bigint;
+  epoch: bigint;
+  effective: bigint;
+  activating: bigint;
+  deactivating: bigint;
 }
 
 const HUGE_NUM = 1_000_000_000_000_000n;
 
 test('activating', (t) => {
-    const targetEpoch = 11n;
-    const stake = 10n;
-    const delegation = {
-        stake,
-        activationEpoch: targetEpoch,
-        deactivationEpoch: HUGE_NUM,
-        unused: 0n,
-        voterPubkey: new Uint8Array(),
-    };
-    const stakeHistory = [
-        {
-            epoch: targetEpoch - 1n,
-            effective: HUGE_NUM,
-            activating: HUGE_NUM,
-            deactivating: HUGE_NUM,
-        }
-    ];
-    const status = getStakeActivatingAndDeactivating(delegation, targetEpoch, stakeHistory);
-    t.is(status.activating, stake);
-    t.is(status.effective, 0n);
-    t.is(status.deactivating, 0n);
+  const targetEpoch = 11n;
+  const stake = 10n;
+  const delegation = {
+    stake,
+    activationEpoch: targetEpoch,
+    deactivationEpoch: HUGE_NUM,
+    unused: 0n,
+    voterPubkey: new Uint8Array(),
+  };
+  const stakeHistory = [
+    {
+      epoch: targetEpoch - 1n,
+      effective: HUGE_NUM,
+      activating: HUGE_NUM,
+      deactivating: HUGE_NUM,
+    },
+  ];
+  const status = getStakeActivatingAndDeactivating(
+    delegation,
+    targetEpoch,
+    stakeHistory
+  );
+  t.is(status.activating, stake);
+  t.is(status.effective, 0n);
+  t.is(status.deactivating, 0n);
 });
 test('effective', (t) => {
-    const targetEpoch = 11n;
-    const stake = 10n;
-    const delegation = {
-        stake,
-        activationEpoch: targetEpoch - 1n,
-        deactivationEpoch: HUGE_NUM,
-        unused: 0n,
-        voterPubkey: new Uint8Array(),
-    };
-    const stakeHistory = [
-        {
-            epoch: targetEpoch - 1n,
-            effective: HUGE_NUM,
-            activating: stake,
-            deactivating: HUGE_NUM,
-        }
-    ];
-    const status = getStakeActivatingAndDeactivating(delegation, targetEpoch, stakeHistory);
-    t.is(status.activating, 0n);
-    t.is(status.effective, stake);
-    t.is(status.deactivating, 0n);
+  const targetEpoch = 11n;
+  const stake = 10n;
+  const delegation = {
+    stake,
+    activationEpoch: targetEpoch - 1n,
+    deactivationEpoch: HUGE_NUM,
+    unused: 0n,
+    voterPubkey: new Uint8Array(),
+  };
+  const stakeHistory = [
+    {
+      epoch: targetEpoch - 1n,
+      effective: HUGE_NUM,
+      activating: stake,
+      deactivating: HUGE_NUM,
+    },
+  ];
+  const status = getStakeActivatingAndDeactivating(
+    delegation,
+    targetEpoch,
+    stakeHistory
+  );
+  t.is(status.activating, 0n);
+  t.is(status.effective, stake);
+  t.is(status.deactivating, 0n);
 });
 test('deactivating', (t) => {
-    const targetEpoch = 11n;
-    const stake = 10n;
-    const delegation = {
-        stake,
-        activationEpoch: targetEpoch - 1n,
-        deactivationEpoch: targetEpoch,
-        unused: 0n,
-        voterPubkey: new Uint8Array(),
-    };
-    const stakeHistory = [
-        {
-            epoch: targetEpoch - 1n,
-            effective: HUGE_NUM,
-            activating: stake,
-            deactivating: stake,
-        }
-    ];
-    const status = getStakeActivatingAndDeactivating(delegation, targetEpoch, stakeHistory);
-    t.is(status.activating, 0n);
-    t.is(status.effective, stake);
-    t.is(status.deactivating, stake);
+  const targetEpoch = 11n;
+  const stake = 10n;
+  const delegation = {
+    stake,
+    activationEpoch: targetEpoch - 1n,
+    deactivationEpoch: targetEpoch,
+    unused: 0n,
+    voterPubkey: new Uint8Array(),
+  };
+  const stakeHistory = [
+    {
+      epoch: targetEpoch - 1n,
+      effective: HUGE_NUM,
+      activating: stake,
+      deactivating: stake,
+    },
+  ];
+  const status = getStakeActivatingAndDeactivating(
+    delegation,
+    targetEpoch,
+    stakeHistory
+  );
+  t.is(status.activating, 0n);
+  t.is(status.effective, stake);
+  t.is(status.deactivating, stake);
 });
 test('multi-epoch activation', (t) => {
-    const targetEpoch = 11n;
-    const stake = HUGE_NUM;
-    const delegation = {
-        stake,
-        activationEpoch: targetEpoch - 1n,
-        deactivationEpoch: HUGE_NUM,
-        unused: 0n,
-        voterPubkey: new Uint8Array(),
-    };
-    const stakeHistory = [
-        {
-            epoch: targetEpoch - 1n,
-            effective: HUGE_NUM,
-            activating: HUGE_NUM,
-            deactivating: HUGE_NUM,
-        }
-    ];
-    const status = getStakeActivatingAndDeactivating(delegation, targetEpoch, stakeHistory);
-    // all of the total amount activating, but only 9% allowed, so it'll activate 9%
-    const effective = stake * 9n / 100n;
-    t.is(status.activating, stake - effective);
-    t.is(status.effective, effective);
-    t.is(status.deactivating, 0n);
+  const targetEpoch = 11n;
+  const stake = HUGE_NUM;
+  const delegation = {
+    stake,
+    activationEpoch: targetEpoch - 1n,
+    deactivationEpoch: HUGE_NUM,
+    unused: 0n,
+    voterPubkey: new Uint8Array(),
+  };
+  const stakeHistory = [
+    {
+      epoch: targetEpoch - 1n,
+      effective: HUGE_NUM,
+      activating: HUGE_NUM,
+      deactivating: HUGE_NUM,
+    },
+  ];
+  const status = getStakeActivatingAndDeactivating(
+    delegation,
+    targetEpoch,
+    stakeHistory
+  );
+  // all of the total amount activating, but only 9% allowed, so it'll activate 9%
+  const effective = (stake * 9n) / 100n;
+  t.is(status.activating, stake - effective);
+  t.is(status.effective, effective);
+  t.is(status.deactivating, 0n);
 });
 test('multi-epoch deactivation', (t) => {
-    const targetEpoch = 11n;
-    const stake = HUGE_NUM;
-    const delegation = {
-        stake,
-        activationEpoch: targetEpoch - 2n,
-        deactivationEpoch: targetEpoch - 1n,
-        unused: 0n,
-        voterPubkey: new Uint8Array(),
-    };
-    const stakeHistory = [
-        {
-            epoch: targetEpoch - 2n,
-            effective: HUGE_NUM * 100n, // make sure it all activates in one epoch
-            activating: stake,
-            deactivating: stake,
-        },
-        {
-            epoch: targetEpoch - 1n,
-            effective: HUGE_NUM,
-            activating: HUGE_NUM,
-            deactivating: HUGE_NUM,
-        }
-    ];
-    const status = getStakeActivatingAndDeactivating(delegation, targetEpoch, stakeHistory);
-    // all of the total amount deactivating, but only 9% allowed, so it'll deactivate 9%
-    const deactivated = stake * 9n / 100n;
-    t.is(status.activating, 0n);
-    t.is(status.effective, stake - deactivated);
-    t.is(status.deactivating, stake - deactivated);
+  const targetEpoch = 11n;
+  const stake = HUGE_NUM;
+  const delegation = {
+    stake,
+    activationEpoch: targetEpoch - 2n,
+    deactivationEpoch: targetEpoch - 1n,
+    unused: 0n,
+    voterPubkey: new Uint8Array(),
+  };
+  const stakeHistory = [
+    {
+      epoch: targetEpoch - 2n,
+      effective: HUGE_NUM * 100n, // make sure it all activates in one epoch
+      activating: stake,
+      deactivating: stake,
+    },
+    {
+      epoch: targetEpoch - 1n,
+      effective: HUGE_NUM,
+      activating: HUGE_NUM,
+      deactivating: HUGE_NUM,
+    },
+  ];
+  const status = getStakeActivatingAndDeactivating(
+    delegation,
+    targetEpoch,
+    stakeHistory
+  );
+  // all of the total amount deactivating, but only 9% allowed, so it'll deactivate 9%
+  const deactivated = (stake * 9n) / 100n;
+  t.is(status.activating, 0n);
+  t.is(status.effective, stake - deactivated);
+  t.is(status.deactivating, stake - deactivated);
 });
