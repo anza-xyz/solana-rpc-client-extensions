19ce8f7950ce12d3932bab927a6cea401351cda4
Jon C <me@jonc.dev>
2024-10-11 22:26:50 +0200

    js-v1: Various fixups
    #### Problem
    
    The web3.js v1 package is inconsistent with the v2 package.
    
    #### Summary of changes
    
    A few things:
    
    * Rename web3js-1.0 -> js-v1
    * Add testing to CI
    * Format the code
    * Use eslint correctly
    * Fix stake account discriminants

---
+++ js-v1/src/rpc.ts
@@ -0,0 +1,74 @@
+import { Connection, PublicKey } from '@solana/web3.js';
+import { getStakeAccount, getStakeHistory } from './stake';
+import { getStakeActivatingAndDeactivating } from './delegation';
+
+export interface StakeActivation {
+  status: string;
+  active: bigint;
+  inactive: bigint;
+}
+
+export async function getStakeActivation(
+  connection: Connection,
+  stakeAddress: PublicKey
+): Promise<StakeActivation> {
+  const SYSVAR_STAKE_HISTORY_ADDRESS = new PublicKey(
+    'SysvarStakeHistory1111111111111111111111111'
+  );
+  const [epochInfo, { stakeAccount, stakeAccountLamports }, stakeHistory] =
+    await Promise.all([
+      connection.getEpochInfo(),
+      (async () => {
+        const stakeAccountParsed =
+          await connection.getParsedAccountInfo(stakeAddress);
+        if (stakeAccountParsed === null || stakeAccountParsed.value === null) {
+          throw new Error('Account not found');
+        }
+        const stakeAccount = getStakeAccount(stakeAccountParsed);
+        const stakeAccountLamports = stakeAccountParsed.value.lamports;
+        return { stakeAccount, stakeAccountLamports };
+      })(),
+      (async () => {
+        const stakeHistoryParsed = await connection.getParsedAccountInfo(
+          SYSVAR_STAKE_HISTORY_ADDRESS
+        );
+        if (stakeHistoryParsed === null) {
+          throw new Error('StakeHistory not found');
+        }
+        return getStakeHistory(stakeHistoryParsed);
+      })(),
+    ]);
+
+  const { effective, activating, deactivating } = stakeAccount.stake
+    ? getStakeActivatingAndDeactivating(
+        stakeAccount.stake.delegation,
+        BigInt(epochInfo.epoch),
+        stakeHistory
+      )
+    : {
+        effective: BigInt(0),
+        activating: BigInt(0),
+        deactivating: BigInt(0),
+      };
+
+  let status;
+  if (deactivating > 0) {
+    status = 'deactivating';
+  } else if (activating > 0) {
+    status = 'activating';
+  } else if (effective > 0) {
+    status = 'active';
+  } else {
+    status = 'inactive';
+  }
+  const inactive =
+    BigInt(stakeAccountLamports) -
+    effective -
+    stakeAccount.meta.rentExemptReserve;
+
+  return {
+    status,
+    active: effective,
+    inactive,
+  };
+}
